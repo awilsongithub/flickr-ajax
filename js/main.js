495c923e1fafbea09cf3eae00c137fa1f25c3725
Adam Wilson <adamwilson1234@gmail.com>
2018-09-05 22:58:11 -0400

    finished. fetches JSON.

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var tag;
+var flickrAPI = 'https://api.flickr.com/services/feeds/photos_public.gne';
+flickrAPI += '?jsoncallback=?'; // add this to url for jsonp requests 
+
+$(document).ready(function(){
+    registerHandlers();
+})
+
+function registerHandlers(){
+    $('button').click(function(){
+        tag = $(this).text();
+        getPics(tag);
+    })
+}
+
+function getPics(){
+    var data = {
+        tags: tag,
+        format: 'json'
+    }
+    $.getJSON(flickrAPI, data).done(showPics);
+}
+
+function showPics(data){
+    console.log(data);
+    $('.collection').empty();
+    $.each(data.items, function(i, item){
+        var li = '<li class="collection-item avatar">';
+        li += '<img src="' + item.media.m + '" ';
+        li += 'class="circle">';
+        li += '<span class="title">' + tag + ' ' + i;
+        li += '</span></li>';
+        $('.collection').append(li);
+    })
+    return false;
+}
